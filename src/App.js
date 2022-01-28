42d32a4c56dc9751ca0c4323737fddaf39c8a0f7
kuro <93817520+EIJIKUROKAWA@users.noreply.github.com>
2022-01-28 18:45:58 +0900

    todoアプリ実装

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return <div className="app">
     <Header />
     <TaskCards />
-    <AddTaskCardButton />
   </div>;
 }
 
--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, } from 'react-beautiful-dnd';
 import { Task } from './Task';
 
+const reorder = (taskList, startIndex, endIndex) => {
+  const remove = taskList.splice(startIndex,1);
+    taskList.splice(endIndex, 0, remove[0]);
+};
+
 export const Tasks = ({ taskList, setTaskList }) => {
+  const handleDragEnd = (result) => {
+    
+    reorder(taskList, result.source.index, result.destination.index);
+    setTaskList(taskList);
+  };
   return(
-      <DragDropContext>
+    <div>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
-           <div>   
-            {taskList.map((task) => (
-             <div>
-               <Task task={task} 
+           <div {...provided.droppableProps} ref={provided.innerRef}>   
+            {taskList.map((task, index) => (
+             <div key={task.id}>
+               <Task 
+               index={index}
+               task={task} 
                taskList={taskList} 
                setTaskList={setTaskList}
                />
              </div>
             ))}
+            {provided.placeholder}
             </div>
           )}
-      </Droppable>
-      </DragDropContext>
+          </Droppable>
+         </DragDropContext>
+      </div>
   );
 };
